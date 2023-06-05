2b4b417f3315e3b79b126ed97f6e29c1c91d2336
Cecilia <dizzystainless@gmail.com>
2023-06-05 13:08:43 +0200

    Revert "Revert "Unit of work beginning""
    This reverts commit 1cf0d97e468e674401aa57b6d5d55f2ef3a5959e.

---
+++ SOLID-DEMO/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Server.UnitOfWork
+{
+    public interface IUnitOfWork<out TContext> where TContext : DbContext, new()
+    {
+        //The following Property is going to hold the context object
+        TContext Context { get; }
+        //Start the database Transaction
+        void CreateTransaction();
+        //Commit the database Transaction
+        void Commit();
+        //Rollback the database Transaction
+        void Rollback();
+        //DbContext Class SaveChanges method
+        void Save();
+    }
+}
